a7788a9fa7e836c7d69b87e9b7bc4aa06ad3f980
Aaron Contreras <aaron@redradix.com>
2017-11-24 17:07:21 +0100

    :art:

---
+++ recipes.js
@@ -0,0 +1,27 @@
+const MealTypes = {
+  Pasta: ['Espagueti pesto'],
+  Pescado: ['Lubina horno'],
+  Arroz: ['Tres delicias', 'Paella'],
+  Verduras: ['Acelgas con garbanzos', 'Menestra'],
+  Carne: ['Guiso de ciervo', 'Chuleton con patatas'],
+  Legumbres: ['Lentejas con chorizo', 'Acelgas con garbanzos']
+};
+
+function isMealType(recipe, mealType) {
+  return MealTypes[mealType].includes(recipe);
+}
+
+function match(menu, template) {
+  if (menu.length !== template.length) return false;
+  return menu.every(({ lunch, dinner }, index) => {
+    const comidaTemplate = template[index].lunch;
+    const cenaTemplate = template[index].dinner;
+    return (
+      isMealType(lunch, comidaTemplate) && isMealType(dinner, cenaTemplate)
+    );
+  });
+}
+
+module.exports = {
+  match
+};
--- test.js
+++ test.js
@@ -1,3 +1,6 @@
+const recipes = require('./recipes');
+const { match } = recipes;
+
 const template = [
   { lunch: 'Pasta', dinner: 'Verduras' },
   { lunch: 'Pescado', dinner: 'Arroz' },
@@ -24,30 +27,6 @@ const menuDiferente = [
   { lunch: 'Acelgas con garbanzos', dinner: 'Guiso de ciervo' },
   { lunch: 'Lentejas con chorizo', dinner: 'Tres delicias' }
 ];
-
-const MealTypes = {
-  Pasta: ['Espagueti pesto'],
-  Pescado: ['Lubina horno'],
-  Arroz: ['Tres delicias', 'Paella'],
-  Verduras: ['Acelgas con garbanzos', 'Menestra'],
-  Carne: ['Guiso de ciervo', 'Chuleton con patatas'],
-  Legumbres: ['Lentejas con chorizo', 'Acelgas con garbanzos']
-};
-
-function isMealType(recipe, mealType) {
-  return MealTypes[mealType].includes(recipe);
-}
-
-function match(menu, template) {
-  if (menu.length !== template.length) return false;
-  return menu.every(({ lunch, dinner }, index) => {
-    const comidaTemplate = template[index].lunch;
-    const cenaTemplate = template[index].dinner;
-    return (
-      isMealType(lunch, comidaTemplate) && isMealType(dinner, cenaTemplate)
-    );
-  });
-}
 
 function createMenu(template) {
   const myMenu = menu.slice();
