1564e6dede8ddf1bcd0e58d7b995fd098d3f4080
Aaron Contreras <aaron@redradix.com>
2017-11-24 17:00:07 +0100

    Match returns false if any meal differs in type

--- test.js
+++ test.js
@@ -1,36 +1,51 @@
 const template = [
-  { Comida: 'Pasta', Cena: 'Verdura' },
-  { Comida: 'Pescado', Cena: 'Arroz' },
-  { Comida: 'Verduras', Cena: 'Carne' },
-  { Comida: 'Legumbres', Cena: 'Arroz' }
+  { lunch: 'Pasta', dinner: 'Verduras' },
+  { lunch: 'Pescado', dinner: 'Arroz' },
+  { lunch: 'Verduras', dinner: 'Carne' },
+  { lunch: 'Legumbres', dinner: 'Arroz' }
 ];
 
 const menu = [
-  { Comida: 'Espagueti pesto', Cena: 'Menestra' },
-  { Comida: 'Lubina horno', Cena: 'Paella' },
-  { Comida: 'Acelgas con garbanzos', Cena: 'Guiso de ciervo' },
-  { Comida: 'Lentejas con chorizo', Cena: 'Tres delicias' }
+  { lunch: 'Espagueti pesto', dinner: 'Menestra' },
+  { lunch: 'Lubina horno', dinner: 'Paella' },
+  { lunch: 'Acelgas con garbanzos', dinner: 'Guiso de ciervo' },
+  { lunch: 'Lentejas con chorizo', dinner: 'Tres delicias' }
 ];
 
 const menuAlternativo = [
-  { Comida: 'Lubina horno', Cena: 'Paella' },
-  { Comida: 'Acelgas con garbanzos', Cena: 'Guiso de ciervo' },
-  { Comida: 'Lentejas con chorizo', Cena: 'Tres delicias' }
+  { lunch: 'Lubina horno', dinner: 'Paella' },
+  { lunch: 'Acelgas con garbanzos', dinner: 'Guiso de ciervo' },
+  { lunch: 'Lentejas con chorizo', dinner: 'Tres delicias' }
 ];
 
 const menuDiferente = [
-  { Comida: 'Lubina horno', Cena: 'Paella' },
-  { Comida: 'Lubina horno', Cena: 'Paella' },
-  { Comida: 'Acelgas con garbanzos', Cena: 'Guiso de ciervo' },
-  { Comida: 'Lentejas con chorizo', Cena: 'Tres delicias' }
+  { lunch: 'Lubina horno', dinner: 'Paella' },
+  { lunch: 'Lubina horno', dinner: 'Paella' },
+  { lunch: 'Acelgas con garbanzos', dinner: 'Guiso de ciervo' },
+  { lunch: 'Lentejas con chorizo', dinner: 'Tres delicias' }
 ];
+
+const MealTypes = {
+  Pasta: ['Espagueti pesto'],
+  Pescado: ['Lubina horno'],
+  Arroz: ['Tres delicias', 'Paella'],
+  Verduras: ['Acelgas con garbanzos', 'Menestra'],
+  Carne: ['Guiso de ciervo', 'Chuleton con patatas'],
+  Legumbres: ['Lentejas con chorizo', 'Acelgas con garbanzos']
+};
+
+function isMealType(recipe, mealType) {
+  return MealTypes[mealType].includes(recipe);
+}
 
 function match(menu, template) {
   if (menu.length !== template.length) return false;
-  return menu.every(({ Comida, Cena }, index) => {
-    const comidaTemplate = template[index].Comida;
-    const cenaTemplate = template[index].Cena;
-    return isMealType(Comida, comidaTemplate) && isMealType(Cena, cenaTemplate);
+  return menu.every(({ lunch, dinner }, index) => {
+    const comidaTemplate = template[index].lunch;
+    const cenaTemplate = template[index].dinner;
+    return (
+      isMealType(lunch, comidaTemplate) && isMealType(dinner, cenaTemplate)
+    );
   });
 }
 
