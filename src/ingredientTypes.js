725c37e1deb2f72f782aa0e57467b071da2c0451
Miguel <miguel@redradix.com>
2018-01-19 15:44:39 +0100

    added ingredient types and categorized ingredients

---
+++ src/ingredientTypes.js
@@ -0,0 +1,36 @@
+export const cereales = 'cereales';
+export const tuberculos= 'tuberculos'
+export const azucares = 'azucares'
+export const sales = 'sales'
+export const grasasVegetales = 'grasas vegetales'
+export const grasasAnimales = 'grasas animales'
+export const lacteos = 'lacteos'
+export const carne = 'carne'
+export const pescado = 'pescado'
+export const huevos = 'huevos'
+export const legumbres = 'legumbres'
+export const frutosSecos = 'frutos secos'
+export const verduras = 'verduras'
+export const hortalizas = 'hortalizas'
+export const frutas = 'frutas'
+export const condimentos = 'condimentos'
+
+
+
+export default [
+  cereales, 
+  tuberculos, 
+  azucares,
+  grasasAnimales, 
+  grasasVegetales, 
+  lacteos,
+  carne,
+  pescado,
+  huevos,
+  legumbres,
+  frutosSecos,
+  verduras,
+  hortalizas,
+  frutas,
+  condimentos
+];
--- src/ingredients/fridge.js
+++ src/ingredients/fridge.js
@@ -1,13 +1,15 @@
-export const judiasVerdes = "judias verdes";
-export const tomate = "tomate";
-export const nata = "nata";
-export const coliflor = "coliflor";
-export const leche = "leche";
-export const queso = "queso";
-export const pollo = "Pollo de corral";
-export const salmon = "salmon";
-export const fresas = "fresas";
-export const chorizo = "chorizo para guisar";
-export const panceta = "Panceta";
-export const alcachofa = "Alcachofas";
-export const parmesano = "Parmesano";
+import { verduras, frutas, lacteos, carne, pescado } from "../ingredientTypes";
+
+export const judiasVerdes = {name: "judias verdes", type: verduras}
+export const tomate = {name: "tomate", type: frutas}
+export const nata = {name: "nata", type: lacteos}
+export const coliflor = {name: "coliflor", type: verduras}
+export const leche = {name: "leche", type: lacteos}
+export const queso = {name: "queso", type: lacteos}
+export const pollo = {name: "Pollo de corral", type: carne}
+export const salmon = {name: "salmon", type: pescado}
+export const fresas = {name: "fresas", type: frutas}
+export const chorizo = {name: "chorizo para guisar", type: carne}
+export const panceta = {name: "Panceta", type: carne}
+export const alcachofa = {name: "Alcachofas", type: verduras}
+export const parmesano = {name: "Parmesano", type: lacteos}
