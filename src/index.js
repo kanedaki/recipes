4c10b1ada2eaf991b327994dfa8163953f71a0f5
Miguel <miguel@redradix.com>
2018-01-19 14:53:41 +0100

    fix import on index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { createMenu } from "./recipes";
-import shoppingList from "./shoppingList";
+import {getShoppingList} from "./shoppingList";
 import normalTemplate from "./templates/normal";
 import { printMenu, printShoppingList } from "./print";
 
 const menu = createMenu(normalTemplate);
 printMenu(menu);
-const list = shoppingList(menu);
+const list = getShoppingList(menu);
 printShoppingList(list);
