063c70ebd50cfd642f737e9e83c699bfd200921b
Miguel <miguel@redradix.com>
2018-01-19 14:46:28 +0100

    add recipes

---
+++ src/recipes/ensalada-couscous.js
@@ -0,0 +1,22 @@
+import { arroz } from "../mealTypes";
+import { alcachofa } from "../ingredients/fridge";
+import {
+  arrozBlanco,
+  sal,
+  aceite,
+  zanahoria,
+  ajo,
+} from "../ingredients/pantry";
+
+export default {
+  mealType: arroz,
+  name: "Ensalada de cous cous",
+  ingredients: [
+    { ingredient: sal },
+    { ingredient: aceite },
+    { ingredient: zanahoria, qty: { amount: "3" } },
+    { ingredient: alcachofa, qty: { amount: "4" } },
+    { ingredient: arrozBlanco, qty: { amount: "50", units: "g" } },
+  ],
+  steps: ["Todo el mundo sabe como hacer paella"],
+};
---
+++ src/recipes/vichissoise.js
@@ -0,0 +1,22 @@
+import { verduras} from "../mealTypes";
+import { alcachofa } from "../ingredients/fridge";
+import {
+  arrozBlanco,
+  sal,
+  aceite,
+  zanahoria,
+  ajo,
+} from "../ingredients/pantry";
+
+export default {
+  mealType: verduras,
+  name: "Vichissoise",
+  ingredients: [
+    { ingredient: sal },
+    { ingredient: aceite },
+    { ingredient: zanahoria, qty: { amount: "3" } },
+    { ingredient: alcachofa, qty: { amount: "4" } },
+    { ingredient: arrozBlanco, qty: { amount: "50", units: "g" } },
+  ],
+  steps: ["Todo el mundo sabe como hacer paella"],
+};
