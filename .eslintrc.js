47fab52b024d4ff3f7de469afd637b8d69dae0ae
Arturo <arturo@redradix.com>
2018-03-12 12:00:42 +0100

    disable comma dagle rule in linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,11 @@ module.exports = {
     semi: ['error', 'never'],
     quotes: ['error', 'single'],
     ['import/prefer-default-export']: 0,
-    ['dangle-comma']: 0,
+    'comma-dangle': 0,
     ['no-shadow']: 0,
     ["no-mixed-operators"]: 0,
     ["no-restricted-syntax"]: 0,
+    'function-paren-newline': 0
   },
   "parserOptions": {
     "ecmaVersion": 2018
