a9e617b4bc6d5fd690e4c6e63845a8a72b92a539
y4izus <yaiza.garcia.mm@gmail.com>
2018-04-17 14:44:37 +0200

    ingredients autocomplete

--- front-admin/src/recipes.js
+++ front-admin/src/recipes.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { List, Edit, Create, Datagrid, ReferenceField, TextField, EditButton, DisabledInput, LongTextInput, ReferenceInput, required, SelectInput, SimpleForm, TextInput } from 'admin-on-rest';
+import { List, Edit, Create, Datagrid, ReferenceField, TextField, EditButton, DisabledInput, LongTextInput, ReferenceInput, required, SelectInput, SimpleForm, TextInput, AutocompleteInput } from 'admin-on-rest';
+import AutoComplete from 'material-ui/AutoComplete';
 
 export const RecipeList = (props) => (
     <List {...props}>
@@ -14,13 +15,22 @@ const RecipeTitle = ({ record }) => {
     return <span>Recipe {record ? `"${record.name}"` : ''}</span>;
 };
 
+const choices = [
+    { _id: 123, full_name: 'Leo Tolstoi', sex: 'M' },
+    { _id: 456, full_name: 'Jane Austen', sex: 'F' },
+];
+
 export const RecipeEdit = (props) => (
     <Edit title={<RecipeTitle />} {...props}>
         <SimpleForm>
             <DisabledInput source="id" />
             
             <TextInput source="name" />
-            
+
+            <ReferenceInput label="Ingredient" source="ingredient_id" reference="ingredients" allowEmpty>
+                <AutocompleteInput optionText="name" optionValue="id"  options={{ filter: AutoComplete.caseInsensitiveFilter }}/>
+            </ReferenceInput>
+
         </SimpleForm>
     </Edit>
 );
