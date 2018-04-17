a763d27ca169ed7fa060767737b7f84493e8b27a
Fran Bosquet <fran@redradix.com>
2018-04-17 15:50:40 +0200

    Fixed tests

--- src/tests/business-logic.test.js
+++ src/tests/business-logic.test.js
@@ -3,7 +3,7 @@ import { light } from '../businessLogic/enums/activity'
 import { male } from '../businessLogic/enums/sex'
 import { lunch, breakfast } from '../businessLogic/enums/meals'
 import { getRandomNumber } from '../utils'
-import { findMatchingRecipe } from '../businessLogic/recipe'
+import { findMatchingRecipes } from '../businessLogic/recipe'
 import recipe1 from './mocks/recipe1'
 import recipe2 from './mocks/recipe2'
 
@@ -48,32 +48,29 @@ describe('business logic', () => {
       const currentMenu = [{ lunch: recipe1 }]
       const dayRecipes = []
       const meal = lunch
-      const recipe = await findMatchingRecipe(userRecipes, currentMenu, dayRecipes, meal)
+      const recipe = await findMatchingRecipes(userRecipes, currentMenu, dayRecipes, meal)
       expect(recipe).toEqual(recipe2)
     })
     it('not find a recipe matching meal', async () => {
       const currentMenu = [{ lunch: recipe1 }]
       const dayRecipes = []
       const meal = breakfast
-      const recipe = await findMatchingRecipe(userRecipes, currentMenu, dayRecipes, meal)
+      const recipe = await findMatchingRecipes(userRecipes, currentMenu, dayRecipes, meal)
       expect(recipe).toEqual(undefined)
     })
     it('not find a recipe not included already in menu', async () => {
       const currentMenu = [{ lunch: recipe1, dinner: recipe2 }]
       const dayRecipes = []
       const meal = lunch
-      const recipe = await findMatchingRecipe(userRecipes, currentMenu, dayRecipes, meal)
+      const recipe = await findMatchingRecipes(userRecipes, currentMenu, dayRecipes, meal)
       expect(recipe).toEqual(undefined)
     })
     it('not find a recipe not included already in day recipes', async () => {
       const currentMenu = [{ lunch: recipe1 }]
       const dayRecipes = { dinner: recipe2 }
       const meal = lunch
-      const recipe = await findMatchingRecipe(userRecipes, currentMenu, dayRecipes, meal)
+      const recipe = await findMatchingRecipes(userRecipes, currentMenu, dayRecipes, meal)
       expect(recipe).toEqual(undefined)
     })
-  })
-  describe('shopping list', () => {
-    it('')
   })
 })
