86db806da03634ceb747ee46318b691492e30ab1
y4izus <yaiza.garcia.mm@gmail.com>
2018-04-18 12:44:25 +0200

    Add calories Input field

--- front-admin/src/modules/ingredients/components/IngredientCaloriesInfo.js
+++ front-admin/src/modules/ingredients/components/IngredientCaloriesInfo.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {TextInput} from 'admin-on-rest'
+import { Field } from 'redux-form' 
 
 export const IngredientCaloriesInfo = ({ value, unit }) => {
     return (
-        <div>
-            Calories {value} {unit}
-        </div>
+        <Field 
+            name='general.calories.value'
+            type='number'
+            component={TextInput}
+            source='calories'
+        >
+        </Field>
     )
 }
 
