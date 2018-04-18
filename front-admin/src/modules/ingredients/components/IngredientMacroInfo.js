c0f853ea2cd591bc670042d58a0e45a404005b10
y4izus <yaiza.garcia.mm@gmail.com>
2018-04-18 12:47:00 +0200

    Add collapsing function to Macro component

--- front-admin/src/modules/ingredients/components/IngredientMacroInfo.js
+++ front-admin/src/modules/ingredients/components/IngredientMacroInfo.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import { IngredientMacroNutrient } from './IngredientMacroNutrient'
 
-export const IngredientMacroInfo = ({ macroInfo }) => {
-    const macroNames = Object.keys(macroInfo)
-    return (
-        <div>
-            <h2>Macronutrients</h2>
-            {macroNames.map(el => <IngredientMacroNutrient key={el} name={el} value={macroInfo[el].value} unit={macroInfo[el].unit}/>)}
-        </div>
-    )
+export class IngredientMacroInfo extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            collapsed: true
+        }
+        this.toggleCollapse = this.toggleCollapse.bind(this)
+    }
+
+    
+    render(){
+        const { macroInfo} = this.props
+        const macroNames = Object.keys(macroInfo)
+        return (
+            <div>
+                <h2>Macronutrients</h2>
+                <button type='button' onClick={this.toggleCollapse}>{ this.state.collapsed ? 'Expand' : 'Collapse'}</button>
+                {!this.state.collapsed && macroNames.map(el => <IngredientMacroNutrient key={el} name={el} value={macroInfo[el].value} unit={macroInfo[el].unit}/>)}
+            </div>
+        )
+    }
+
+    toggleCollapse(e){
+        this.setState({
+            collapsed: !this.state.collapsed
+        })
+    }
 }
