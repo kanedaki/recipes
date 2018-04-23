b11a71824796484444444eadc5479c3f6866bba4
y4izus <yaiza.garcia.mm@gmail.com>
2018-04-23 16:03:52 +0200

    [Front] Show recipe seasons and meals with chips

--- front-admin/src/modules/recipes/RecipeEdit.js
+++ front-admin/src/modules/recipes/RecipeEdit.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import { TextField, List, Datagrid, EditButton, Edit, DisabledInput, ReferenceInput, required, SimpleForm, TextInput, AutocompleteInput } from 'admin-on-rest'
+import AutoComplete from 'material-ui/AutoComplete'
+import { Edit, ReferenceInput, required, SimpleForm, TextInput, AutocompleteInput, SelectArrayInput } from 'admin-on-rest'
 import { Field } from 'redux-form' 
 
 import { RecipeTitle } from './components/RecipeTitle'
 import { RecipeIngrList } from './components/RecipeIngrList'
 import { RecipeAddIngredient } from './components/RecipeAddIngredient'
 
-import AutoComplete from 'material-ui/AutoComplete'
+const seasonsChoices = [{ season: "winter" }, { season: "spring" }, { season: "summer" }, { season: "autumn" }]
+const mealChoices = [{ meal: "lunch" }, { meal: "dinner" }, { meal: "breakfast" }]
 
 export const RecipeEdit = (props) => {
     return (
     <Edit title={<RecipeTitle />} {...props}>
         <SimpleForm>
-            <DisabledInput source="id" />
             <TextInput source="name" validate={required}/>
+            <SelectArrayInput source="seasons" choices={seasonsChoices} optionText="season" optionValue="season"/>  
+            <SelectArrayInput source="meal" choices={mealChoices} optionText="meal" optionValue="meal"/>          
             <RecipeIngrList/>
 
             <ReferenceInput label="Ingredient" source="ingredient_id" reference="ingredients" allowEmpty>
