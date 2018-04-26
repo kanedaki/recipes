274e8d38c0991f9fa057e71a99987fcf9b1a802d
y4izus <yaiza.garcia.mm@gmail.com>
2018-04-26 09:31:27 +0200

    [Front] Delete unused code. Unit on IngredientNutrient is not required

--- front-admin/src/modules/ingredients/components/IngredientNutrient.js
+++ front-admin/src/modules/ingredients/components/IngredientNutrient.js
@@ -3,14 +3,6 @@ import PropTypes from 'prop-types'
 import { Field } from 'redux-form'
 import { TextInput } from 'admin-on-rest'
 
-const renderField = props => (
-  <div className="renderField">
-    <label>{props.label}</label>
-    <input {...props.input} type={props.type} placeholder={0} />
-    {props.touched && props.error && <span>{props.error}</span>}
-  </div>
-)
-
 export const IngredientNutrient = ({ cat, subCat, subCatName, label, value, unit }) => {
   return (
     <Field
@@ -26,7 +18,7 @@ export const IngredientNutrient = ({ cat, subCat, subCatName, label, value, unit
 IngredientNutrient.propTypes = {
   name: PropTypes.string,
   value: PropTypes.number,
-  unit: PropTypes.string.isRequired
+  unit: PropTypes.string,
 }
 
 IngredientNutrient.defaultProps = {
